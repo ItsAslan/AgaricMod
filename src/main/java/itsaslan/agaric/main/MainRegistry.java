3386fc1ba627c162916422f47b5eaee6e0e7106b
Christian Teichmann <ckt082207@gmail.com>
2024-04-05 19:46:15 -0400

    First Commit

---
+++ src/main/java/itsaslan/agaric/main/MainRegistry.java
@@ -0,0 +1,37 @@
+package itsaslan.agaric.main;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import itsaslan.agaric.lib.ModVars;
+import itsaslan.agaric.proxy.CommonProxy;
+
+@Mod(modid = ModVars.MOD_ID, name = ModVars.MOD_NAME, version = ModVars.MOD_VERSION)
+public class MainRegistry
+{
+    @Mod.Instance
+    public static MainRegistry instance = new MainRegistry();
+
+    @SidedProxy(clientSide = "itsaslan.agaric.proxy.ClientProxy", serverSide = "itsaslan.agaric.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent $e)
+    {
+        proxy.preInit($e);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent $e)
+    {
+        proxy.init($e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent $e)
+    {
+        proxy.postInit($e);
+    }
+}
